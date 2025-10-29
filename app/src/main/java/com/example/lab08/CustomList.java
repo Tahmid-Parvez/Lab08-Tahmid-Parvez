3a13663b1cbec568dd42cd20a58feb51c880cb25
Tahmid Parvez <tahmidparvez343@gmail.com>
2025-10-28 18:17:44 -0600

    did all the tests

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -4,17 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomList {
-    private List<City> cities;
+    private ArrayList<City> cities;
 
     public CustomList() {
         this.cities = new ArrayList<>();
+
     }
 
+    public boolean hasCity(City city){
+
+        for (int i = 0; i < cities.size(); i ++){
+            if (city == cities.get(i)){
+                return true;
+            }
+        }
+
+        return false;
+    }
     public void addCity(City city) {
         cities.add(city);
     }
 
-    
+    public void deleteCity(City city){
+        cities.remove(city);
+
+    }
+
+    public int size() {
+        return cities.size();
+    }
+
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,56 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 public class CustomListTest {
+
+    private CustomList cities;
+
+    @Test
+    public void testHasCity(){
+
+        cities = new CustomList();
+
+        City city = new City("Calgary", "AB");
+
+        cities.addCity(city);
+
+        assert cities.hasCity(city);
+
+    }
+
+    @Test
+    public void testDeleteCity(){
+        cities = new CustomList();
+
+        City city = new City("Calgary", "AB");
+
+        cities.addCity(city);
+
+        assert cities.hasCity(city);
+
+        cities.deleteCity(city);
+
+        cities.deleteCity(city);
+
+        assert !cities.hasCity(city);
+
+    }
+
+    @Test
+    public void testCitiesCount(){
+
+        cities = new CustomList();
+
+        City city = new City("Calgary", "AB");
+
+        cities.addCity(city);
+
+        assert cities.size() == 1;
+
+
+    }
 
 }
